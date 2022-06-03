64f0817043a9e2b105ed4af51831d3ba528d382f
DevMeena <68868864+DevMeena@users.noreply.github.com>
2022-06-04 00:52:57 +0530

    google-auth merged

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import './App.css';
 import SignInSide from './Components/SignInSide.js';
 import SignUpSide from './Components/SignUpSide.js';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import GoogleAuth from './Components/GoogleLogin';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <div className="content">
+      <div className='App'>
+        <div className='content'>
           <Routes>
-            <Route exact path="/" element ={<SignInSide/>}>
-            </Route>
-            
-            <Route exact path="/signup" element ={<SignUpSide/>}>
-            </Route>
+            <Route exact path='/' element={<SignInSide />}></Route>
+            <Route exact path='/google-auth' element={<GoogleAuth />}></Route>
+            <Route exact path='/signup' element={<SignUpSide />}></Route>
           </Routes>
         </div>
       </div>
---
+++ src/Components/GoogleLogin.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import { gapi } from 'gapi-script';
+import './style.css';
+
+const clientId =
+  '644150943784-dd4aaim7fuvgemorumocbbcgb4rmvdel.apps.googleusercontent.com';
+
+gapi.load('client:auth2', () => {
+  gapi.client.init({
+    clientId:
+      '644150943784-dd4aaim7fuvgemorumocbbcgb4rmvdel.apps.googleusercontent.com',
+    plugin_name: 'chat',
+  });
+});
+
+const GoogleAuth = () => {
+  const [showloginButton, setShowloginButton] = useState(true);
+  const [showlogoutButton, setShowlogoutButton] = useState(false);
+
+  const onLoginSuccess = (res) => {
+    console.log('Login Success:', res.profileObj);
+    setShowloginButton(false);
+    setShowlogoutButton(true);
+  };
+
+  const onLoginFailure = (res) => {
+    console.log('Login Failed:', res);
+  };
+
+  const onSignoutSuccess = () => {
+    alert('You have been logged out successfully');
+    console.clear();
+    setShowloginButton(true);
+    setShowlogoutButton(false);
+  };
+
+  return (
+    <div>
+      {showloginButton ? (
+        <GoogleLogin
+          clientId={clientId}
+          buttonText='Sign In'
+          onSuccess={onLoginSuccess}
+          onFailure={onLoginFailure}
+          cookiePolicy={'single_host_origin'}
+          isSignedIn={true}
+        />
+      ) : null}
+
+      {showlogoutButton ? (
+        <GoogleLogout
+          clientId={clientId}
+          buttonText='Sign Out'
+          onLogoutSuccess={onSignoutSuccess}
+        ></GoogleLogout>
+      ) : null}
+    </div>
+  );
+};
+
+export default GoogleAuth;
