705ad16b28bf49254662529d6b0730be3c01aefd
DevMeena <68868864+DevMeena@users.noreply.github.com>
2022-06-04 21:36:31 +0530

    resolved home conflict

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import Home from "./pages/home/Home.js";
+import Home from './pages/home/Home.js';
 import SignInSide from './Components/SignInSide.js';
 import SignUpSide from './Components/SignUpSide.js';
-import Home from './Components/Home.js';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import GoogleAuth from './Components/GoogleLogin';
 import PrivateRoute from './Components/PrivateRoute';
@@ -12,12 +11,10 @@ function App() {
       <div className='App'>
         <div className='content'>
           <Routes>
-            <Route exact path="/" element ={<SignInSide/>}>
-            </Route>            
-            <Route exact path="/signup" element ={<SignUpSide/>}></Route> 
             <Route exact path='/' element={<SignInSide />}></Route>
-            <Route exact path='/google-auth' element={<GoogleAuth />}></Route>
             <Route exact path='/signup' element={<SignUpSide />}></Route>
+            <Route exact path='/google-auth' element={<GoogleAuth />}></Route>
+
             <Route element={<PrivateRoute />}>
               <Route exact path='/home' element={<Home />}></Route>
             </Route>
