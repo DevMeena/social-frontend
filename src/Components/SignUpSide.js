97488fe445e48fdb38209bb476907371eb17e85c
DevMeena <68868864+DevMeena@users.noreply.github.com>
2022-06-04 21:39:09 +0530

    fixed signup link

--- src/Components/SignUpSide.js
+++ src/Components/SignUpSide.js
@@ -160,7 +160,7 @@ export default function SignUpSide() {
             </Button>
             <Grid container justifyContent='flex-end'>
               <Grid item>
-                <Link href='#' variant='body2'>
+                <Link href='/' variant='body2'>
                   Already have an account? Sign in
                 </Link>
               </Grid>
