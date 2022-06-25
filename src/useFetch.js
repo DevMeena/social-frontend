94e9167f263f3c753284ed7f7b282f2c38288763
DevMeena <68868864+DevMeena@users.noreply.github.com>
2022-06-25 11:59:32 +0530

    added custom hook

---
+++ src/useFetch.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { API } from './api';
+
+function useFetch(url) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(API + url)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [url]);
+
+  const refetch = () => {
+    setLoading(true);
+    axios
+      .get(API + url)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  return { data, loading, error, refetch };
+}
+
+export default useFetch;
